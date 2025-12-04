fefc0ff9f96fc4f1c7a3e8ff7b359b99a5444e34
RoxelGaming <artur215rx@wp.pl>
2025-12-04 23:39:20 +0100

    all

--- MLZ - OLD/Strona mlż/js/header.js
+++
@@ -1,9 +0,0 @@
-const body = document.querySelector("body"),
-      menus = body.querySelector(".menus"),
-      ListRoll = body.querySelector(".ListRoll");
-
-menus.addEventListener("click", () => {
-
-    ListRoll.classList.toggle("invisible"); 
-
-})
--- MLZ - OLD/Strona mlż/js/js.js
+++
@@ -1,9 +0,0 @@
-const bodyE = document.querySelector("body"),
-      lightButton = bodyE.querySelector(".lightButton");
-
-lightButton.addEventListener("click", () => {
-
-    bodyE.classList.toggle("light"); 
-
-})
-
